ba16969fef2f1bbb2b78e582dbef7e3d3b2923e5
murakamikaze <murakamikaze@validao.xyz>
2024-10-02 13:50:54 +0900

    fix find new files

--- internal/monitors/block_monitor.go
+++ internal/monitors/block_monitor.go
@@ -3,6 +3,7 @@ package monitors
 import (
 	"bufio"
 	"encoding/json"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -19,97 +20,116 @@ func StartBlockMonitor(cfg config.Config) {
 	go func() {
 		blockTimeDir := filepath.Join(cfg.NodeHome, "hl/data/block_times")
 		var latestFile string
+		var fileOffset int64 = 0
 		for {
-			var err error
-			latestFile, err = utils.GetLatestFile(blockTimeDir)
+			newLatestFile, err := utils.GetLatestFile(blockTimeDir)
 			if err != nil {
 				log.Printf("Error finding latest block time file: %v", err)
 				time.Sleep(5 * time.Second)
 				continue
 			}
 
-			processBlockTimeFile(latestFile)
-			time.Sleep(5 * time.Second)
+			if newLatestFile != latestFile {
+				log.Printf("Switching to new block time file: %s", newLatestFile)
+				latestFile = newLatestFile
+				fileOffset = 0
+			}
+
+			fileOffset = processBlockTimeFile(latestFile, fileOffset)
+			time.Sleep(3 * time.Second)
 		}
 	}()
 }
 
-func processBlockTimeFile(filePath string) {
+func processBlockTimeFile(filePath string, offset int64) int64 {
 	file, err := os.Open(filePath)
 	if err != nil {
 		log.Printf("Error opening block time file %s: %v", filePath, err)
-		return
+		return offset
 	}
 	defer file.Close()
 
+	_, err = file.Seek(offset, 0)
+	if err != nil {
+		log.Printf("Error seeking in file %s: %v", filePath, err)
+		return offset
+	}
+
 	reader := bufio.NewReader(file)
+	lineCount := 0
 	for {
 		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
 		if err != nil {
-			time.Sleep(1 * time.Second)
-			continue
+			log.Printf("Error reading line from block time file %s: %v", filePath, err)
+			break
 		}
 		parseBlockTimeLine(line)
+		lineCount++
+		offset += int64(len(line))
 	}
+
+	log.Printf("Processed %d new lines from block time file %s", lineCount, filePath)
+	return offset
 }
 
-
 func parseBlockTimeLine(line string) {
-    var data map[string]interface{}
-    err := json.Unmarshal([]byte(line), &data)
-    if err != nil {
-        log.Printf("Error parsing block time line: %v", err)
-        return
-    }
+	var data map[string]interface{}
+	err := json.Unmarshal([]byte(line), &data)
+	if err != nil {
+		log.Printf("Error parsing block time line: %v", err)
+		return
+	}
 
-    // Variables to hold the parsed values
-    var height float64
-    var applyDuration float64
+	// Variables to hold the parsed values
+	var height float64
+	var applyDuration float64
 
-    // Extract and parse 'height'
-    if heightValue, ok := data["height"]; ok {
-        switch v := heightValue.(type) {
-        case float64:
-            height = v
-        case string:
-            height, err = strconv.ParseFloat(v, 64)
-            if err != nil {
-                log.Printf("Error parsing height value '%v' to float64: %v", v, err)
-                return
-            }
-        default:
-            log.Printf("Unexpected type for 'height': %T", v)
-            return
-        }
-    } else {
-        log.Printf("'height' key not found in data")
-        return
-    }
+	// Extract and parse 'height'
+	if heightValue, ok := data["height"]; ok {
+		switch v := heightValue.(type) {
+		case float64:
+			height = v
+		case string:
+			height, err = strconv.ParseFloat(v, 64)
+			if err != nil {
+				log.Printf("Error parsing height value '%v' to float64: %v", v, err)
+				return
+			}
+		default:
+			log.Printf("Unexpected type for 'height': %T", v)
+			return
+		}
+	} else {
+		log.Printf("'height' key not found in data")
+		return
+	}
 
-    // Extract and parse 'apply_duration'
-    if applyDurationValue, ok := data["apply_duration"]; ok {
-        switch v := applyDurationValue.(type) {
-        case float64:
-            applyDuration = v
-        case string:
-            applyDuration, err = strconv.ParseFloat(v, 64)
-            if err != nil {
-                log.Printf("Error parsing apply_duration value '%v' to float64: %v", v, err)
-                return
-            }
-        default:
-            log.Printf("Unexpected type for 'apply_duration': %T", v)
-            return
-        }
-    } else {
-        log.Printf("'apply_duration' key not found in data")
-        return
-    }
+	// Extract and parse 'apply_duration'
+	if applyDurationValue, ok := data["apply_duration"]; ok {
+		switch v := applyDurationValue.(type) {
+		case float64:
+			applyDuration = v
+		case string:
+			applyDuration, err = strconv.ParseFloat(v, 64)
+			if err != nil {
+				log.Printf("Error parsing apply_duration value '%v' to float64: %v", v, err)
+				return
+			}
+		default:
+			log.Printf("Unexpected type for 'apply_duration': %T", v)
+			return
+		}
+	} else {
+		log.Printf("'apply_duration' key not found in data")
+		return
+	}
 
-    // Update Prometheus metrics
-    metrics.HLBlockHeightGauge.Set(height)
-    metrics.HLApplyDurationGauge.Set(applyDuration)
+	// Update metrics
+	metrics.HLBlockHeightGauge.Set(height)
+	metrics.HLApplyDurationGauge.Set(applyDuration)
 
-    // Log the updated metrics
-    log.Printf("Updated block metrics: height=%.0f, apply_duration=%f", height, applyDuration)
+	log.Printf("Updated metrics: height=%.0f, apply_duration=%.6f", height, applyDuration)
 }
--- internal/monitors/proposal_monitor.go
+++ internal/monitors/proposal_monitor.go
@@ -3,9 +3,11 @@ package monitors
 import (
 	"bufio"
 	"encoding/json"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/validaoxyz/hyperliquid-exporter/internal/config"
@@ -18,39 +20,65 @@ func StartProposalMonitor(cfg config.Config) {
 	go func() {
 		logsDir := filepath.Join(cfg.NodeHome, "hl/data/replica_cmds")
 		var latestFile string
+		var fileOffset int64 = 0
 		for {
-			var err error
-			latestFile, err = utils.GetLatestFile(logsDir)
+			newLatestFile, err := utils.GetLatestFile(logsDir)
 			if err != nil {
 				log.Printf("Error finding latest log file: %v", err)
 				time.Sleep(10 * time.Second)
 				continue
 			}
 
-			processProposalFile(latestFile)
-			time.Sleep(10 * time.Second)
+			if newLatestFile != latestFile {
+				log.Printf("Switching to new proposal log file: %s", newLatestFile)
+				latestFile = newLatestFile
+				fileOffset = 0
+			}
+
+			fileOffset = processProposalFile(latestFile, fileOffset)
+			time.Sleep(3 * time.Second)
 		}
 	}()
 }
 
-func processProposalFile(filePath string) {
+func processProposalFile(filePath string, offset int64) int64 {
 	file, err := os.Open(filePath)
 	if err != nil {
 		log.Printf("Error opening proposal file %s: %v", filePath, err)
-		return
+		return offset
 	}
 	defer file.Close()
 
+	_, err = file.Seek(offset, 0)
+	if err != nil {
+		log.Printf("Error seeking in file %s: %v", filePath, err)
+		return offset
+	}
+
 	reader := bufio.NewReader(file)
+	lineCount := 0
 	for {
 		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
 		if err != nil {
-			time.Sleep(1 * time.Second)
-			continue
+			log.Printf("Error reading line from proposal file %s: %v", filePath, err)
+			break
 		}
 		parseProposalLine(line)
+		lineCount++
+		offset += int64(len(line))
 	}
+
+	log.Printf("Processed %d new lines from proposal file %s", lineCount, filePath)
+	return offset
 }
+
+var (
+	proposerCounts = make(map[string]int)
+	proposerMutex  sync.Mutex
+)
 
 func parseProposalLine(line string) {
 	var data map[string]interface{}
@@ -61,11 +89,34 @@ func parseProposalLine(line string) {
 	}
 
 	abciBlock, ok := data["abci_block"].(map[string]interface{})
-	if ok {
-		proposer, ok := abciBlock["proposer"].(string)
-		if ok {
-			metrics.HLProposerCounter.WithLabelValues(proposer).Inc()
-			log.Printf("Proposer %s counter incremented.", proposer)
-		}
+	if !ok {
+		log.Printf("ABCI block not found in proposal line")
+		return
 	}
+
+	proposer, ok := abciBlock["proposer"].(string)
+	if !ok {
+		log.Printf("Proposer not found in ABCI block")
+		return
+	}
+
+	metrics.HLProposerCounter.WithLabelValues(proposer).Inc()
+
+	// Update our local counter
+	proposerMutex.Lock()
+	proposerCounts[proposer]++
+	count := proposerCounts[proposer]
+	proposerMutex.Unlock()
+
+	log.Printf("Proposer %s counter incremented. Local count: %d", proposer, count)
+}
+
+func GetProposerCounts() map[string]int {
+	proposerMutex.Lock()
+	defer proposerMutex.Unlock()
+	counts := make(map[string]int)
+	for k, v := range proposerCounts {
+		counts[k] = v
+	}
+	return counts
 }
