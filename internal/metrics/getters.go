1e7a0c7c15633f070c1af06d980d1d248b87c9e2
murakamikaze <murakamikaze@validao.xyz>
2024-11-13 14:53:36 +0700

    add support for validator pings

--- internal/metrics/getters.go
+++ internal/metrics/getters.go
@@ -5,3 +5,16 @@ func IsValidator() bool {
 	defer metricsMutex.Unlock()
 	return nodeIdentity.IsValidator
 }
+
+func GetValidatorStakes() map[string]float64 {
+	metricsMutex.RLock()
+	defer metricsMutex.RUnlock()
+
+	stakes := make(map[string]float64)
+	if values, exists := labeledValues[HLValidatorStakeGauge]; exists {
+		for validator, value := range values {
+			stakes[validator] = value.value
+		}
+	}
+	return stakes
+}
